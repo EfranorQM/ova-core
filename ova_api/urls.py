4ff5837905414fe36d9dd8c05cd308e893706360
kevinfraymanrc <kevinfraymanrc@ufps.edu.co>
2024-12-01 19:31:23 -0500

    Agregado handler para la ruta raíz en urls.py

--- ova_api/urls.py
+++ ova_api/urls.py
@@ -1,7 +1,12 @@
+from django.http import HttpResponse
 from django.contrib import admin
 from django.urls import path, include
 
+def home(request):
+    return HttpResponse("Bienvenido a la API OVA")
+
 urlpatterns = [
+    path('', home),  # Ruta para la raíz
     path('admin/', admin.site.urls),
     path('api/', include('ova_core.urls')),  # Incluye las rutas del router
 ]
